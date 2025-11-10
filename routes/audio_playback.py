9aab41896c5e67f260a47429eff5625b9d7c7646
Nico Heinola <nico.heinola@gmail.com>
2025-11-10 22:20:35 +0200

    Refactor audio playback endpoint to accept file uploads instead of raw audio data

--- routes/audio_playback.py
+++ routes/audio_playback.py
@@ -1,5 +1,5 @@
 import os
-from fastapi import APIRouter, Body, Response
+from fastapi import APIRouter, File, UploadFile
 
 from helpers.audio.audio_playback_helper import AudioPlaybackHelper
 from middleware.auth import require_auth
@@ -8,16 +8,17 @@ router = APIRouter()
 
 
 @router.post("/play-audio")
-def play_audio(token: str = require_auth(), body: dict = Body(...)):
+async def play_audio(token: str = require_auth(), file: UploadFile = File(...)):
     """
     Endpoint to play a sound file.
-    Expects raw audio data in the request body.
+    Expects an audio file upload.
     """
 
     # Store in temporary file
     temp_file_path: str = os.path.join("/tmp", "temp_audio_file")
     with open(temp_file_path, "wb") as temp_file:
-        temp_file.write(body.get("audio_data", b""))
+        content = await file.read()
+        temp_file.write(content)
 
     try:
         AudioPlaybackHelper.play_audio(temp_file_path)
